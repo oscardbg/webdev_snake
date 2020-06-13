8667666b5b795bd1260bab5691bae307ea828c41
Dell <user@dell.com>
2020-06-12 19:44:13 -0500

    Render code ready

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-document.addEventListener('DOMContentLoaded', init);
+let lastRenderTime = 0;
+const SNAKE_SPEED = 2;
 
-function init(){
+function main(currentTime){
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRendr = (currentTime - lastRenderTime) / 1000;
+    if(secondsSinceLastRendr < 1 / SNAKE_SPEED) return;
     
+    console.log('render');
+    lastRenderTime = currentTime;
 }
+
+window.requestAnimationFrame(main)
