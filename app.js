dcdea5d48997bb7edbef3203b191bbd7527b02aa
Dell <user@dell.com>
2020-06-12 22:28:06 -0500

    Snake movement validations 1

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js';
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
+const gameBoard = document.querySelector('.board');
 
 function main(currentTime){
     window.requestAnimationFrame(main)
     const secondsSinceLastRendr = (currentTime - lastRenderTime) / 1000;
     if(secondsSinceLastRendr < 1 / SNAKE_SPEED) return;
-    
-    console.log('render');
+        
     lastRenderTime = currentTime;
+
+    update();
+    draw();
 }
 
 window.requestAnimationFrame(main)
+
+function update(){
+    updateSnake();
+}
+function draw(){
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+}
